6278121044e482d2a9a8a36be53892c43b4cdd90
Pedro Valois <mail@phvv.me>
2023-06-16 17:42:35 +0900

    advancing on transform method

--- main.py
+++ main.py
@@ -30,70 +30,26 @@ class Styler:
     img = base64.b64encode(Path("files/template.jpeg").read_bytes()).decode("ascii")
 
     payload = {
-    "init_images": [str(img)],
-    "denoising_strength": 0.75,
-    "image_cfg_scale": 0,
-    "mask_blur": 4,
-    "inpainting_fill": 0,
-    "inpaint_full_res": True,
-    "prompt": "",
-    "seed": -1,
-    "subseed": -1,
-    "subseed_strength": 0,
-    "seed_resize_from_h": -1,
-    "seed_resize_from_w": -1,
-    "batch_size": 2,
-    "n_iter": 1,
-    "steps": 50,
-    "cfg_scale": 7,
-    "width": 512,
-    "height": 512
-    # "resize_mode": 0,
-    # "denoising_strength": 0.75,
-    # "image_cfg_scale": 0,
-    # "mask": "",
-    # "mask_blur": 4,
-    # "inpainting_fill": 0,
-    # "inpaint_full_res": True,
-    # "inpaint_full_res_padding": 0,
-    # "inpainting_mask_invert": 0,
-    # "initial_noise_multiplier": 0,
-    # "prompt": "maltese puppy",
-    # # "styles": [
-    # #   "string"
-    # # ],
-    # "seed": -1,
-    # "subseed": -1,
-    # "subseed_strength": 0,
-    # "seed_resize_from_h": -1,
-    # "seed_resize_from_w": -1,
-    # # "sampler_name": "string",
-    # "batch_size": 1,
-    # "n_iter": 1,
-    # "steps": 5,
-    # "cfg_scale": 7,
-    # "width": 512,
-    # "height": 512,
-    # "restore_faces": False,
-    # "tiling": False,
-    # "do_not_save_samples": False,
-    # "do_not_save_grid": False,
-    # "negative_prompt": "string",
-    # "eta": 0,
-    # "s_min_uncond": 0,
-    # "s_churn": 0,
-    # "s_tmax": 0,
-    # "s_tmin": 0,
-    # "s_noise": 1,
-    # "override_settings": {},
-    # "override_settings_restore_afterwards": True,
-    # "script_args": [],
-    # "sampler_index": "Euler",
-    # "include_init_images": False,
-    # # "script_name": "string",
-    # "send_images": True,
-    # "save_images": False,
-    # "alwayson_scripts": {}
+        "init_images": [img],
+        "prompt": 'a handsome man',
+        "negative_prompt": "",
+        "batch_size": 1,
+        "steps": 20,
+        "cfg_scale": 7,
+        "alwayson_scripts": {
+            "controlnet": {
+                "args": [
+                    {
+                        "module": "canny",
+                        "model": "control_v11p_sd15_canny [d14c016b]",
+                    },
+                    {
+                        "module": "openpose",
+                        "model": "control_v11p_sd15_openpose [cab727d4]",
+                    }
+                ]
+            }
+        }
     }
 
     print("img2img")
@@ -105,9 +61,47 @@ class Styler:
     if "images" not in response:
       print(response)
 
-    for i in response["images"]:
-      image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
-      image.save("transform.png")
+    for i, img in enumerate(response["images"]):
+      image = Image.open(io.BytesIO(base64.b64decode(img.split(",",1)[0])))
+      image.save(f"transform-{i}.png")
+
+  def test_controlnet(self):
+    # Read Image in RGB order
+    encoded_image = base64.b64encode(Path("files/template.jpeg").read_bytes()).decode("ascii")
+    # img = cv2.imread('files/template.jpeg')
+
+    # Encode into PNG and send to ControlNet
+    # retval, bytes = cv2.imencode('.png', img)
+    # encoded_image = base64.b64encode(bytes).decode('utf-8')
+
+    # A1111 payload
+    payload = {
+        "init_images": [encoded_image],
+        "prompt": 'a handsome man',
+        "negative_prompt": "",
+        "batch_size": 1,
+        "steps": 20,
+        "cfg_scale": 7,
+        "alwayson_scripts": {
+            "controlnet": {
+                "args": [
+                    {
+                        "module": "canny",
+                        "model": "control_v11p_sd15_canny [d14c016b]",
+                    }
+                ]
+            }
+        }
+    }
+
+    print("Trigger Generation")
+    response = requests.post(url=f'{url}/sdapi/v1/img2img', json=payload)
+
+    # Read results
+    r = response.json()
+    result = r['images'][0]
+    image = Image.open(io.BytesIO(base64.b64decode(result.split(",", 1)[0])))
+    image.save('output.png')
     
   def test(self):
     payload = {
