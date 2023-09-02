6c26fe150493472a336ea32c4b36b2f3e1f139b6
Your Name <you@example.com>
2023-09-02 11:20:12 +0900

    canny working

--- cli.py
+++ cli.py
@@ -8,7 +8,7 @@ import logging
 import torchvision.transforms.functional as TF
 import kornia
 from controlnet_aux.processor import Processor
-
+import math
 from tqdm import tqdm
 
 from helpers.video import VideoProcessor
@@ -17,9 +17,10 @@ def resize_to_fit(tensor: torch.Tensor) -> torch.Tensor:
     """
     Resize the tensor so that both dimensions are divisible by 8.
     """
+    MUL = 8
     height, width = tensor.shape[-2:]
-    new_height = (height // 8) * 8
-    new_width = (width // 8) * 8
+    new_height = math.ceil(height / MUL) * MUL
+    new_width = math.ceil(width / MUL) * MUL
     return TF.resize(tensor, size=(new_height, new_width))
 
 def correct_offset(images: torch.Tensor, offset: int = 1) -> torch.Tensor:
@@ -58,7 +59,7 @@ class Commands:
         # Initialize necessary objects like ControlNet models and pipeline
         logging.info('Loading ControlNet models...')
         self.controlnets = [
-            ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=self.dtype)
+            ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=self.dtype),
             # ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-depth", torch_dtype=self.dtype),
             # ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-openpose", torch_dtype=self.dtype),
         ]
@@ -80,44 +81,54 @@ class Commands:
     def _process_frames(self, frame_batch: list[np.ndarray], prompt: str, neg_prompt: str = "monochrome, lowres, bad anatomy, worst quality, low quality") -> np.ndarray:
         if not frame_batch:
             return []
-        
-        img = frame_batch[0]
 
-        # return frame_batch
+        t = np.stack(frame_batch)
+        t = torch.from_numpy(t)
+        t = t.to(self.device)
+        t = t.to(self.dtype)
+        t = t.div(255)
+        t = t.permute(0, 3, 1, 2)
+        t = resize_to_fit(t)
 
-        # Convert the frame batch to tensors
-        # input_batch = torch.from_numpy(np.stack(frame_batch)).to(self.device, self.dtype).div(255).permute(0, 3, 1, 2)
-        # shape: (frame, channel, height, width)
+        # img = t[0].mul(255).movedim(0, -1).byte().cpu().numpy()
+        # t_depth = self.depth(img)
+        # t_depth = torch.from_numpy(t_depth).to(self.device, self.dtype).movedim(-1, 0).unsqueeze_(0)
 
-        # t = resize_to_fit(input_batch)
-        # t_canny = kornia.filters.canny(t, hysteresis=False)[0].expand_as(t)
-        # t.mul_(255).permute(0, 2, 3, 1).to(torch.uint8).cpu().numpy()
+        # t_pose = np.array(self.openpose(img))
+        # t_pose = torch.from_numpy(t_pose).to(self.device, self.dtype).movedim(-1, 0).unsqueeze_(0)
 
-        t_canny = self.canny(img)
-        # t_depth = self.depth(img)[np.newaxis, ...]
-        # t_pose = np.array(self.openpose(img))[np.newaxis, ...]
+        t_canny = kornia.filters.canny(t, hysteresis=False)[0].expand_as(t)
+        # t_canny = TF.resize(t_canny, size=t_depth.shape[-2:], antialias=True)
 
-        t = torch.from_numpy(t_canny).unsqueeze_(0).div_(255).movedim(-1, 0)
-        # return TF.resize(t, size=img.shape[:-1], antialias=False).movedim(0, -1).numpy()
+        control_images = [t_canny]  # , t_depth, t_pose
 
-        t = self.pipe(
-            prompt,  # Repeat the prompt for each frame in the batch
-            negative_prompt=neg_prompt,
-            image=[t],
+        # assert t_canny.shape == t_depth.shape == t_pose.shape, "Shapes of inputs must be equal"
+
+        n = t.size(0)
+        out = self.pipe(
+            [prompt] * n,  # Repeat the prompt for each frame in the batch
+            negative_prompt=[neg_prompt] * n,
+            image=control_images,
             num_inference_steps=5,
             generator=self.generator,
             output_type="pt"
         ).images
 
-        return TF.resize(t, size=img.shape[:-1], antialias=True).movedim(1, -1).cpu().numpy()
+        out = TF.resize(out, size=t.shape[-2:], antialias=True)
+        out = out.mul(255)
+        out = out.permute(0, 2, 3, 1)
+        out = out.to(torch.uint8)
+        out = out.cpu()
+        out = out.numpy()
+        return out
     
     def convert(self, input_path: str, output_path: str, prompt: str) -> None:
         with VideoProcessor(input_path, output_path, batch_size=1) as video:
             for i, frames in enumerate(video):
                 converted_frames = self._process_frames(frames, prompt)
                 video.write(converted_frames)
-
-                if i > 10:
+                
+                if i > 5:
                     break
 
 # example usage:
